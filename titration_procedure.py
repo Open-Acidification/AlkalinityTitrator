e617b54eaf4b27cd028512d074a6673c833768e6
kadensu <ksukachevin@gmail.com>
2020-02-06 15:53:43 -0800

    WIP: Updated titration procedure and architecture

--- titration_procedure.py
+++ titration_procedure.py
@@ -5,12 +5,17 @@ Walla Walla University
 '''
 # Libraries
 from time import sleep
+from datetime import datetime
+
 from gpiozero as gpio
 
 import board 
 import busio
 import digitalio
 import adafruit_max31865
+
+# for use with I2C
+import smbus
 
 # CircuitPython? For communicating with PT1000 sensor
 # Before continuing make sure your board's lib folder or root filesystem has the adafruit_max31865.mpy, and adafruit_bus_device files and folders copied over.
@@ -41,8 +46,44 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.D5)
 sensor = adafruit_max31865.MAX31865(spi, cs, wires=3, rtd_nominal=1000.0, ref_resistor=4300.0)
 
-display_options()
+test_readings()
 
+bus = smbus.SMBus(0)
+address = 0x60
+
+def bearing255():
+        bear = bus.read_byte_data(address, 1)
+        return bear
+
+def bearing3599():
+        bear1 = bus.read_byte_data(address, 2)
+        bear2 = bus.read_byte_data(address, 3)
+        bear = (bear1 << 8) + bear2
+        bear = bear/10.0
+        return bear
+
+while True:
+        bearing = bearing3599()     #this returns the value to 1 decimal place in degrees. 
+        bear255 = bearing255()      #this returns the value as a byte between 0 and 255. 
+        print bearing
+        print bear255
+        time.sleep(1)
+
+# Display options to the user (including starting titration)
+# display_options()
+
+def test_readings():
+    while True:
+        print(datetime.now())
+        print("Temperature readings: ")
+        temp, res = read_temperature()
+        print('Temperature: {0:0.3f}C'.format(temp))
+        print('Resistance: {0:0.3f} Ohms'.format(res))
+
+        print("pH readings: ")
+        volt = read_pH()
+        print('Voltage: {0:0.3f}mV'.format(volt))
+        time.sleep(1)
 
 def display_options():
     print("0. Run titration\n1. Calibrate\n2. Settings")  # user options upon startup of system
@@ -60,6 +101,7 @@ def display_options():
 
 def run_titration():
     '''Driver for running the titration'''
+    current_pH = 0  # TODO get this value from GPIO; should this be a global variable or passed from function to function?
     solution_weight, pH_molarity = read_user_values_for_titration()
     fast_titration()
     titrate(TARGET_PH, 0.05)
@@ -86,15 +128,16 @@ def fast_titration(target_pH):
     (2) Begin stirring slowly'''
 
     stir(STIR_SLOW)
-    vol_to
-    while (determine_addition_volume())
-    dispence_HCl()
-
+    vol_to_add = determine_addition_volume(current_pH)
+    while vol_to_add > 0:
+        dispense_HCl(vol_to_add)
+        current_pH = read_pH()
+        vol_to_add = determine_addition_volume(current_pH)
 
     titrate(3.5, 20)
 
 def determine_addition_volume():
-    '''Function for determining how much vol cm^3 HCl should be added to get to the required pH value without passing it'''
+    '''Function for determining how much vol cm^3 HCl should be added to get to the required pH value without passing it; returns 0 if no HCl should be added'''
 
 
 def titrate(pH_target, solution_increment_amount):
@@ -123,7 +166,7 @@ def titrate(pH_target, solution_increment_amount):
         if ((pH_new - pH_old) < STABILIZATION_CONSTANT):
             if (pH_new - pH_target < PH_ACCURACY):
                 break
-            dispence_HCl(solution_increment_amount)
+            dispense_HCl(solution_increment_amount)
         pH_old = pH_new
 
         # TODO store temp, pH values or immediately write to file (might be slow)
@@ -135,7 +178,7 @@ def titrate(pH_target, solution_increment_amount):
 
     while (current_pH - TARGET_PH) > PH_ACCURACY:
         # TODO 
-        dispence_HCl(0.05)
+        dispense_HCl(0.05)
         # TODO measure new pH level; wait until readings have settled
         current_pH = new_pH
         # TODO write out data to csv file
@@ -144,19 +187,29 @@ def titrate(pH_target, solution_increment_amount):
 
 # GPIO in/out Functions
 
-def dispence_HCl(volume):
+def dispense_HCl(volume):
     '''Adds HCl to the solution'''
-    # stepper motor driver needed here
+    # stepper motor driver needed here; will likely connect to the Arduino
 
 
 def read_pH():
     '''Reads and returns the pH value from GPIO'''
+    # NOTE this function should wait and make sure pH is stable before returning a value
+    
+
+    return pH
+
+
+def convert_pH_to_voltage():
+    '''Since we don't want to be converting the voltage readings to equivalent pH values each time, we will go the other way (when converting user input pH values and for readability within the code)'''
+
 
 
 def read_temperature():
     '''Reads and returns the temperature from GPIO'''
-    print('Temperature: {0:0.3f}C'.format(sensor.temperature))
-    print('Resistance: {0:0.3f} Ohms'.format(sensor.resistance))
+    # print('Temperature: {0:0.3f}C'.format(sensor.temperature))
+    # print('Resistance: {0:0.3f} Ohms'.format(sensor.resistance))
+    return sensor.temperature, sensor.resistance
 
 
 def stir(speed):
