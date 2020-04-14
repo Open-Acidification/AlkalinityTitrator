8d617d71cbeaec97043a7de5045c257563790c5d
kadensu <ksukachevin@gmail.com>
2020-04-14 21:38:52 +0100

    Pumptest BCM pin 24

--- test functions/pumptest.py
+++ test functions/pumptest.py
@@ -2,15 +2,18 @@
 # pin map https://pinout.xyz/
 import RPi.GPIO as GPIO
 import time
+PIN_NUMBER = 24
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(PIN_NUMBER, GPIO.OUT)
 
 while(True):
     user_input = input("Press 'a' to pulse the pump; 'q' to exit.\n")
     if user_input == 'a':
-        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(PIN_NUMBER, GPIO.HIGH)
         print('Pulse')
     elif user_input == 'q':
         break
+    time.sleep(3)
+    GPIO.output(PIN_NUMBER, GPIO.LOW)
 GPIO.cleanup()  # use this instead
