b64ce9376d3e119d73238fcdc06e02426a312346
kadensu <ksukachevin@gmail.com>
2020-05-11 17:01:29 -0400

    Removed unnecessary functions

--- test functions/read_store_values.py
+++
@@ -1,9 +0,0 @@
-import csv
-from datetime import datetime
-
-
-def write_csv(data_to_write):
-    file_name = datetime.strftime(datetime.now(), '%m-%d-%Y %H:%M:%S:%f') + '.csv'
-    with open(file_name, mode='w') as open_file:
-        data_writer = csv.writer(open_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        # TODO convert data_to_write to csv
