54e66d6f50150d5a1576ac31b7c99f56b1b0eb14
kadensu <ksukachevin@gmail.com>
2020-06-04 16:22:52 -0700

    Removed git install and added pipenv install to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update              # To get the latest package lists
 apt-get upgrade
-apt-get install git  -y     # git
 apt install pipenv -y       # pipenv virual environment
 apt-get install rpi.gpio
+pipenv install
