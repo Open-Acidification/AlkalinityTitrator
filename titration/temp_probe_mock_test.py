e4661bda02a9d48967d81967d4f229afb68e13cd
Konrad McClure <konradmcclure@gmail.com>
2021-08-10 17:32:57 -0700

    * renamed test probe mock function

--- titration/temp_probe_mock_test.py
+++ titration/temp_probe_mock_test.py
@@ -30,7 +30,7 @@ def test_temp_probe_set_temperature():
     temp_sensor.mock_set_temperature(25)
     assert temp_sensor.temperature()
 
-def test_temp_probe_resistance():
+def test_temp_probe_set_resistance():
     temp_sensor = temp_probe_mock.Temp_Probe(board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2)
     temp_sensor.mock_set_resistance(500)
     assert temp_sensor.resistance() == 500
