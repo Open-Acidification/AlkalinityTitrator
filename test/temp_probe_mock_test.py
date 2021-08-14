45ba92125d5802e29f3ba5bc588f07bd91b0cebf
Barun Debnath <barundebnath91@gmail.com>
2021-08-14 22:39:35 +0530

    temp to temperature in temp_probe_mock_test.py

--- test/temp_probe_mock_test.py
+++ test/temp_probe_mock_test.py
@@ -1,54 +1,54 @@
 import src.devices.board_mock as board_mock
-import src.devices.temp_probe_mock as temp_probe_mock
+import src.devices.temperature_probe_mock as temperature_probe_mock
 
 
-def test_temp_probe_create():
-    temp_sensor = temp_probe_mock.Temp_Probe(
+def test_temperature_probe_create():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(
         board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2
     )
-    assert temp_sensor is not None
+    assert temperature_sensor is not None
 
 
-def test_temp_probe_create_null():
-    temp_sensor = temp_probe_mock.Temp_Probe(None, None, None, None)
-    assert temp_sensor is not None
+def test_temperature_probe_create_null():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(None, None, None, None)
+    assert temperature_sensor is not None
 
 
-def test_temp_probe_get_temperature():
-    temp_sensor = temp_probe_mock.Temp_Probe(
+def test_temperature_probe_get_temperature():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(
         board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2
     )
-    assert temp_sensor.get_temperature() == 0
+    assert temperature_sensor.get_temperature() == 0
 
 
-def test_temp_probe_temperature_null():
-    temp_sensor = temp_probe_mock.Temp_Probe(None, None, None, None)
-    assert temp_sensor.get_temperature() == 0
+def test_temperature_probe_temperature_null():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(None, None, None, None)
+    assert temperature_sensor.get_temperature() == 0
 
 
-def test_temp_probe_resistance():
-    temp_sensor = temp_probe_mock.Temp_Probe(
+def test_temperature_probe_resistance():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(
         board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2
     )
-    assert temp_sensor.get_resistance() == 1000.0
+    assert temperature_sensor.get_resistance() == 1000.0
 
 
-def test_temp_probe_resistance_null():
-    temp_sensor = temp_probe_mock.Temp_Probe(None, None, None, None)
-    assert temp_sensor.get_resistance() == 1000.0
+def test_temperature_probe_resistance_null():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(None, None, None, None)
+    assert temperature_sensor.get_resistance() == 1000.0
 
 
-def test_temp_probe_set_temperature():
-    temp_sensor = temp_probe_mock.Temp_Probe(
+def test_temperature_probe_set_temperature():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(
         board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2
     )
-    temp_sensor.mock_set_temperature(25)
-    assert temp_sensor.get_temperature()
+    temperature_sensor.mock_set_temperature(25)
+    assert temperature_sensor.get_temperature()
 
 
-def test_temp_probe_set_resistance():
-    temp_sensor = temp_probe_mock.Temp_Probe(
+def test_temperature_probe_set_resistance():
+    temperature_sensor = temperature_probe_mock.Temperature_Probe(
         board_mock.SCK, board_mock.MOSI, board_mock.MISO, board_mock.D4, wires=2
     )
-    temp_sensor.mock_set_resistance(500)
-    assert temp_sensor.get_resistance() == 500
+    temperature_sensor.mock_set_resistance(500)
+    assert temperature_sensor.get_resistance() == 500
