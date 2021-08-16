4fb3ae6c8167ac62ca2c4be9c0882eb380366b61
Konrad <konradmcclure@gmail.com>
2021-08-16 14:11:41 -0700

    forgot to save constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -107,7 +107,7 @@ calibration_data_format = {
     "vol_pump": 0,
 }
 
-CALIBRATION_FILENAME = "src/calibration.json"
+CALIBRATION_FILENAME = "../data/calibration.json"
 # temperature calibration values
 TEMPERATURE_REF_RESISTANCE = 4300.0
 TEMPERATURE_NOMINAL_RESISTANCE = 1000.0
