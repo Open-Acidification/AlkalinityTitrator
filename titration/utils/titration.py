0b91e04f9f1038b3badc8a6b2e7698b338db8be7
Konrad <konradmcclure@gmail.com>
2021-08-18 21:07:51 -0700

    Found that the KeyboardInterrupt exception inherents from BaseException, not Exception, and thus won't trigger the SSR turning off

--- titration/utils/titration.py
+++ titration/utils/titration.py
@@ -33,14 +33,14 @@ def run():
         interfaces.temperature_controller.deactivate()
         interfaces.lcd_clear()
         interfaces.ui_lcd.lcd_backlight(False)
-    except Exception:
+    except BaseException:
         # Deactivate the SSR if any crash occurs
         if interfaces.temperature_controller is not None:
             interfaces.temperature_controller.deactivate()
         print("\n************************\nDeactivated SSR\n************************")
 
-        #print(sys.exc_info()[0])
-        #traceback.print_exc()
+        print(sys.exc_info()[0])
+        traceback.print_exc()
 
 
 def initialize_components():
