90cc464970989d03164ea073b6583e7dd9a0169d
Konrad <konradmcclure@gmail.com>
2021-08-18 21:04:29 -0700

    confirming that SSR is getting shut down for sure

--- titration/utils/titration.py
+++ titration/utils/titration.py
@@ -37,10 +37,10 @@ def run():
         # Deactivate the SSR if any crash occurs
         if interfaces.temperature_controller is not None:
             interfaces.temperature_controller.deactivate()
-        print("\nDeactivated SSR")
+        print("\n************************\nDeactivated SSR\n************************")
 
-        print(sys.exc_info()[0])
-        traceback.print_exc()
+        #print(sys.exc_info()[0])
+        #traceback.print_exc()
 
 
 def initialize_components():
