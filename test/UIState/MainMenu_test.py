355a50d34e9ff9b2ceb9fa3f7e7718ba433a185c
Kieran Sukachevin <kieran@Kierans-MacBook-Pro-2.local>
2022-02-06 18:32:43 -0800

    MainMenu test edited

--- test/UIState/MainMenu_test.py
+++ test/UIState/MainMenu_test.py
@@ -1,11 +1,11 @@
 import pytest
 from io import StringIO
 from titration.utils.UIState import MainMenu
+from titration.utils.Titrator import Titrator
 
-def test_handleKey(monkeypatch):
-    key_input = StringIO('*\n')
-    monkeypatch.setattr('sys.stdin', key_input)
-    mainMenu = MainMenu()
+def test_handleKey():
+    key_input = '*'
+    mainMenu = MainMenu.MainMenu(Titrator())
     mainMenu.handleKey(key_input)
     assert(mainMenu.routineSelection == 2)
     mainMenu.handleKey(key_input)
