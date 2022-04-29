130eea7677bdf51405d6ee3a3a23b1f7ae4075ed
Kieran Sukachevin <kieran@Kierans-MacBook-Pro-2.local>
2022-04-28 21:35:47 -0700

    test file removed

--- test/utils/titrator_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from io import StringIO
-from titration.utils.titrator import Titrator
-
-def test_loop(monkeypatch):
-    pass
-    # with monkeypatch.context() as m:
-    #     monkeypatch.setattr('sys.stdin', StringIO('A\n'))
-    #     Titrator().loop()
