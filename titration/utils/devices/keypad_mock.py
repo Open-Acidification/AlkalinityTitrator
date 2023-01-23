9288b89933787fff97b77173388539a6f73208ce
Taylor Smith <tjs6282000@hotmail.com>
2023-01-23 12:20:43 -0800

    Fixed Linter Errors

--- titration/utils/devices/keypad_mock.py
+++ titration/utils/devices/keypad_mock.py
@@ -3,6 +3,7 @@ Module for mocking the keypad.py Keypad() class for testing purposes
 """
 import click
 
+
 class Keypad:
     def __init__(self, r0, r1, r2, r3, c0, c1, c2, c3):
         # Flag for emulating the button being pressed and then released
