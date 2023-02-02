89946492b2615701dffcfe62eeee34ab7c2468bd
Taylor Smith <83837157+TaylorSmith28@users.noreply.github.com>
2023-02-02 09:36:45 -0800

    Seperated the keypad from interfaces (#85)
    * Updating testing
    
    * Updating tests more
    
    * Seperated the keypad from interfaces
    
    * Updated keypad_mock.py
    
    * Fixed Linter Error
    
    * Resolving merge issues with linting
    
    * Fixed the linter error
    
    * Fixed linter errors
    
    ---------
    
    Co-authored-by: Preston Carman <preston.carman@wallawalla.edu>

--- titration/utils/devices/keypad_mock.py
+++ titration/utils/devices/keypad_mock.py
@@ -1,13 +1,23 @@
 """
-Module for mocking the keypad.py Keypad() class for testing purposes
+The file for mock Keypad class
 """
 import click
 
 
 class Keypad:
+    """
+    This is a class for the mock Keypad class
+    """
+
     def __init__(self, r0, r1, r2, r3, c0, c1, c2, c3):
-        # Flag for emulating the button being pressed and then released
+        """
+        The constructor for the mock Keypad class.
+        The Parameters are the board pins that the keypad uses
+        """
         pass
 
     def keypad_poll(self):
+        """
+        The function to poll a keyboard press
+        """
         return click.getchar()
