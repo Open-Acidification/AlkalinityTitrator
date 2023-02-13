071cc60bfb5e0e3ab5b3b10dbf8c1326c1364684
Taylor Smith <tjs6282000@hotmail.com>
2023-02-12 16:19:43 -0800

    Removed old code

--- titration/utils/devices/syringe_pump_mock.py
+++ titration/utils/devices/syringe_pump_mock.py
@@ -1,6 +1,11 @@
-import titration.utils.analysis as analysis
 import titration.utils.constants as constants
 import titration.utils.devices.serial_mock as serial_mock
+
+MAX_PUMP_CAPACITY = 1.1
+NUM_CYCLES = {0.05: 470, 1: 9550}
+
+# 1 mL is 9550 pump cycles
+CYCLES_VOLUME_RATIO = 9550
 
 
 class Syringe_Pump:
@@ -72,14 +77,14 @@ class Syringe_Pump:
             if volume > space_in_pump:
                 raise Exception("FILLING ERROR")
             else:
-                cycles = analysis.determine_pump_cycles(volume)
+                cycles = self.__determine_pump_cycles(volume)
                 self.drive_step_stick(cycles, direction)
                 self.volume_in_pump += volume
         elif direction == 1:
             if volume > self.volume_in_pump:
                 raise Exception("PUMPING ERROR")
             else:
-                cycles = analysis.determine_pump_cycles(volume)
+                cycles = self.__determine_pump_cycles(volume)
                 offset = self.drive_step_stick(cycles, direction)
                 # offset is what is returned from drive_step_stick which originally is returned from the arduino
                 if offset != 0:
@@ -108,3 +113,18 @@ class Syringe_Pump:
                 return int(temp)
         else:
             raise Exception("ARDUINO UNAVAILABLE")
+
+    def __determine_pump_cycles(self, volume_to_add):
+        """
+        The function to determines the number of cycles to move given volume
+        Parameters:
+            volume_to_add (int): amount of volume to add in mL
+        Returns:
+            number of cycles (int)
+        """
+        if volume_to_add in NUM_CYCLES:
+            return NUM_CYCLES[volume_to_add]
+        if volume_to_add > 1.1:
+            return 0
+        pump_cycles = CYCLES_VOLUME_RATIO * volume_to_add
+        return int(pump_cycles)
