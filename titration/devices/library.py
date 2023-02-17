95616e9a14b012b7ba7c1aabf984d56b01621bd9
Taylor Smith <tjs6282000@hotmail.com>
2023-02-17 10:09:34 -0800

    Final Linter Push

--- titration/devices/library.py
+++ titration/devices/library.py
@@ -2,18 +2,16 @@
 The file to configure mock objects
 """
 
-# pylint: disable=unused-import
+# pylint: disable=unused-import, ungrouped-imports
 
 from titration import constants
 
 if constants.IS_TEST:
-    from titration.devices import (
-        ads_mock as ADS,
-        analog_mock as analog_in,
-        board_mock as board,
-        i2c_mock as busio,
-        pwm_out_mock as pwmio,
-    )
+    from titration.devices import ads_mock as ADS
+    from titration.devices import analog_mock as analog_in
+    from titration.devices import board_mock as board
+    from titration.devices import i2c_mock as busio
+    from titration.devices import pwm_out_mock as pwmio
     from titration.devices.keypad_mock import Keypad
     from titration.devices.liquid_crystal_mock import LiquidCrystal
     from titration.devices.serial_mock import Serial
