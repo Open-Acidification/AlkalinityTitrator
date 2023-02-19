ace2d796848449f7945ff2dbbd634376fb00271c
Taylor Smith <tjs6282000@hotmail.com>
2023-02-19 11:38:12 -0800

    Updated imports

--- titration/utils/devices/temperature_probe.py
+++ titration/utils/devices/temperature_probe.py
@@ -11,9 +11,9 @@ if constants.IS_TEST:
     from titration.utils.devices.max31865_mock import MAX31865
     from titration.utils.devices.spi_mock import SPI
 else:
-    from digitalio import DigitalInOut
-    from adafruit_max31865 import MAX31865
-    from busio import SPI
+    from digitalio import DigitalInOut  # type: ignore
+    from adafruit_max31865 import MAX31865  # type: ignore
+    from busio import SPI  # type: ignore
 
 
 class TemperatureProbe:
