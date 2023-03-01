84e6f3888315a6d06a39472feb614384a0b75d00
Taylor Smith <tjs6282000@hotmail.com>
2023-03-01 13:55:12 -0800

    Fixed Tests

--- test/devices/temperature_probe_test.py
+++ test/devices/temperature_probe_test.py
@@ -20,14 +20,6 @@ def test_temperature_probe_create():
     assert temperature_sensor is not None
 
 
-def test_temperature_probe_create_null():
-    """
-    The function to test creating a null mock temperature sensor
-    """
-    temperature_sensor = TemperatureProbe(None, None, None, None)
-    assert temperature_sensor is not None
-
-
 def test_temperature_probe_get_temperature():
     """
     The function to test getting a temperature from the mock sensor
@@ -36,25 +28,9 @@ def test_temperature_probe_get_temperature():
     assert temperature_sensor.get_temperature() == 0
 
 
-def test_temperature_probe_temperature_null():
-    """
-    The function to test getting a temperature from a null mock sensor
-    """
-    temperature_sensor = TemperatureProbe(None, None, None, None)
-    assert temperature_sensor.get_temperature() == 0
-
-
 def test_temperature_probe_resistance():
     """
     The function to test getting a resistance from a mock sensor
     """
     temperature_sensor = create_temperature_probe()
-    assert temperature_sensor.get_resistance() == 1000.0
-
-
-def test_temperature_probe_resistance_null():
-    """
-    The function to test getting a resistance from a null mock sensor
-    """
-    temperature_sensor = TemperatureProbe(None, None, None, None)
     assert temperature_sensor.get_resistance() == 1000.0
