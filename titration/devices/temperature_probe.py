2125c14c7f2a84f9098e50330bbd2b3a4e866307
Taylor Smith <tjs6282000@hotmail.com>
2023-05-18 14:38:36 -0700

    New test

--- titration/devices/temperature_probe.py
+++ titration/devices/temperature_probe.py
@@ -6,8 +6,8 @@ The file for the temperature probe device
 
 from titration.devices.library import MAX31865, SPI, DigitalInOut, board
 
-DEFAULT_REF_RESISTANCE = 4300.0
-NOMINAL_RESISTANCE = 1000.0
+#DEFAULT_REF_RESISTANCE = 4300.0
+#NOMINAL_RESISTANCE = 1000.0
 
 # Constants for calibration
 A = 0.0039083
@@ -36,11 +36,9 @@ class TemperatureProbe:
             self.spi,
             self.c_s,
             wires=3,
-            rtd_nominal=NOMINAL_RESISTANCE,
-            ref_resistor=DEFAULT_REF_RESISTANCE,
         )
 
-        self.reference_resistance = DEFAULT_REF_RESISTANCE
+        self.reference_resistance = None
 
     def get_temperature(self):
         """
@@ -63,31 +61,26 @@ class TemperatureProbe:
         Parameters:
             temp (float): reference temperature inputted by the user in celsius
         """
-        # Looping may need to occur to pinpoint the temperature?
-        x = self.sensor.temperature
-        print(x)
-        while ((x - 1) >= temp) or (temp >= (x + 1)):
-            # Temperature below 0 C
-            if temp >= 0:
-                temp = NOMINAL_RESISTANCE * (1 + A * temp + B * temp**2)
-
-            # Temperature above 0 C
-            else:
-                temp = NOMINAL_RESISTANCE * (
-                    1 + A * temp + B * temp**2 + C * (temp - 100) * temp**3
-                )
-
-            # Calculate new reference temperature
-            diff = temp - self.sensor.resistance
-            self.reference_resistance = (
-                self.reference_resistance + diff * self.reference_resistance / temp
+        # Temperature below 0 C
+        if temp >= 0:
+            
+            temp = self.sensor.rtd_nominal * (1 + A * temp + B * temp**2)
+        # Temperature above 0 C
+        else:
+            temp = self.sensor.rtd_nominal * (
+                1 + A * temp + B * temp**2 + C * (temp - 100) * temp**3
             )
 
-            # Reinitialize the device with the new setting
-            self.sensor = MAX31865(
-                self.spi,
-                self.c_s,
-                wires=3,
-                rtd_nominal=NOMINAL_RESISTANCE,
-                ref_resistor=self.reference_resistance,
-            )
+        # Calculate new reference temperature
+        diff = temp - self.sensor.resistance
+        self.reference_resistance = (
+            self.reference_resistance + diff * self.reference_resistance / temp
+        )
+
+        # Reinitialize the device with the new setting
+        self.sensor = MAX31865(
+            self.spi,
+            self.c_s,
+            wires=3,
+            ref_resistor=self.reference_resistance,
+        )
